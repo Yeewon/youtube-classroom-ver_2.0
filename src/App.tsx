0ef1232e300363d8af50fe402f7b519e3a10df93
Yeewon <yes3427@kookmin.ac.kr>
2022-04-23 23:54:12 +0900

    Env: App.tsx 타입스크립트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { ReactElement } from "react";
 
-const App = () => {
-  return <div className="App"></div>;
+const App = (): ReactElement => {
+  return <div className="App" />;
 };
 
 export default App;
