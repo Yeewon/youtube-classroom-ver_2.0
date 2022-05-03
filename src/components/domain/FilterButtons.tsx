28303e91cb41ffd396e28760097f8796af916390
Yeewon <yes3427@kookmin.ac.kr>
2022-05-03 14:37:58 +0900

    Feat: FilterButtons 컴포넌트 구현

---
+++ src/components/domain/FilterButtons.tsx
@@ -0,0 +1,13 @@
+import FilterButton from "../../components/base/FilterButton";
+
+const FilterButtons = () => {
+  return (
+    <>
+      <FilterButton>👁️ 볼 영상</FilterButton>
+      <FilterButton>✅ 본 영상</FilterButton>
+      <FilterButton>👍🏻 좋아요 한 영상</FilterButton>
+    </>
+  );
+};
+
+export default FilterButtons;
