7bef18ca984f4c06d5d28602b4b248fe2a3eb360
Yeewon <yes3427@kookmin.ac.kr>
2022-05-03 14:45:51 +0900

    Feat: Classroom 컴포넌트 구현

---
+++ src/components/domain/Classroom.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import FilterButtons from "../../components/domain/FilterButtons";
+
+const Button = styled.button``;
+
+const Classroom = () => {
+  return (
+    <>
+      <FilterButtons />
+      <Button>🔍 동영상 검색</Button>
+    </>
+  );
+};
+
+export default Classroom;
