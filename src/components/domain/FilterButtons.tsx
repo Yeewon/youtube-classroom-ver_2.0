773363f31515c058369c91f4f04ddc1694d91e3b
Yeewon <yes3427@kookmin.ac.kr>
2022-05-04 17:48:55 +0900

    Style: FilterButtons 컴포넌트 스타일 적용

--- src/components/domain/FilterButtons.tsx
+++ src/components/domain/FilterButtons.tsx
@@ -1,12 +1,17 @@
+import styled from "@emotion/styled";
 import Button from "../base/Button";
+
+const FilterButtonsContainer = styled.div`
+  margin-right: 50px;
+`;
 
 const FilterButtons = () => {
   return (
-    <>
+    <FilterButtonsContainer>
       <Button>👁️ 볼 영상</Button>
       <Button>✅ 본 영상</Button>
       <Button>👍🏻 좋아요 한 영상</Button>
-    </>
+    </FilterButtonsContainer>
   );
 };
 
