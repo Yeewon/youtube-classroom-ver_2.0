299b632a75b0621e90d9a2b84d7b884b029b9148
Yeewon <yes3427@kookmin.ac.kr>
2022-05-04 17:48:36 +0900

    Style: Classroom 컴포넌트 스타일 적용

--- src/components/domain/Classroom.tsx
+++ src/components/domain/Classroom.tsx
@@ -2,12 +2,17 @@ import styled from "@emotion/styled";
 import FilterButtons from "../../components/domain/FilterButtons";
 import Button from "../../components/base/Button";
 
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const Classroom = () => {
   return (
-    <>
+    <ButtonContainer>
       <FilterButtons />
       <Button>🔍 동영상 검색</Button>
-    </>
+    </ButtonContainer>
   );
 };
 
