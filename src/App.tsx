e5379c4cf0952792acd2b51ee90861b64b6dc0f8
Yeewon <yes3427@kookmin.ac.kr>
2022-05-23 23:29:54 +0900

    Feat: 메인 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React, { ReactElement } from "react";
+import Header from "./components/base/Header";
+import Classroom from "./components/domain/Classroom";
 
 const App = (): ReactElement => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Header />
+      <Classroom />
+    </div>
+  );
 };
 
 export default App;
